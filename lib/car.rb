475df38cc8ff59b9944b14069045fff6e2cf0ec0
Gigi M <mmgamao1@gmail.com>
2019-04-01 15:22:38 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,22 @@
  require_relative "./vehicle.rb"
+# car
+#   #new
+#     initializes with two arguments, wheel size and number (FAILED - 1)
+
+class Car  
+  attr_accessor :wheel_size, :wheel_number 
+  
+def initialize(wheel_size, wheel number)
+  @wheel_size = wheel_size
+  @wheel_number = wheel_number 
+end
+end  
+
+#   #wheel_size
+#     has a wheel_size (FAILED - 2)
+#   #wheel_number
+#     has a wheel_number (FAILED - 3)
+#   #go
+#     returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!' (FAILED - 4)
+#   #fill_up_tank
+#     returns 'filling up!' (FAILED - 5)
